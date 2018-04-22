058ad039acfb4d243aa30227cc914e7e01c6c1e6
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-22 12:46:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
-def name=(dog_name)
-  @name = dog_name
-end
-def name
-  @name
+ def name=(dog_name)
+   @name = dog_name
  end
-  def breed=(dog_breed)
+ def name
+   @name
+ end
+ def breed=(dog_breed)
    @breed = dog_breed
-  end
-   def breed
-  @breed
+ end
+ def breed
+   @breed
   end
 end
 
